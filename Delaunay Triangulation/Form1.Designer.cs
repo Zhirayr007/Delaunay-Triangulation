cf5b2275ec44076681090c2fa13f1f550eefa6fe
Zhirayr <jirayr.dabaxyn@mail.ru>
2022-03-14 20:23:28 +0300

    first commit

---
+++ Delaunay Triangulation/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Delaunay_Triangulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.gennods = new System.Windows.Forms.Button();
+            this.Joindots = new System.Windows.Forms.Button();
+            this.Count = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(12, 12);
+            this.picture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(846, 605);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // gennods
+            // 
+            this.gennods.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gennods.Location = new System.Drawing.Point(894, 285);
+            this.gennods.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gennods.Name = "gennods";
+            this.gennods.Size = new System.Drawing.Size(182, 50);
+            this.gennods.TabIndex = 1;
+            this.gennods.Text = "Генерация точек";
+            this.gennods.UseVisualStyleBackColor = true;
+            this.gennods.Click += new System.EventHandler(this.gennods_Click);
+            // 
+            // Joindots
+            // 
+            this.Joindots.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Joindots.Location = new System.Drawing.Point(894, 432);
+            this.Joindots.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Joindots.Name = "Joindots";
+            this.Joindots.Size = new System.Drawing.Size(181, 66);
+            this.Joindots.TabIndex = 2;
+            this.Joindots.Text = "Соединить точки";
+            this.Joindots.UseVisualStyleBackColor = true;
+            this.Joindots.Click += new System.EventHandler(this.Joindots_Click);
+            // 
+            // Count
+            // 
+            this.Count.Location = new System.Drawing.Point(929, 57);
+            this.Count.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(147, 22);
+            this.Count.TabIndex = 3;
+            this.Count.Text = "4";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(904, 120);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Окружности";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(892, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "N= ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.Joindots);
+            this.Controls.Add(this.gennods);
+            this.Controls.Add(this.picture);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button gennods;
+        private System.Windows.Forms.Button Joindots;
+        private System.Windows.Forms.TextBox Count;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Delaunay Triangulation/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delaunay_Triangulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp;
+        Graphics g;
+        Dots[] mas_dots;
+        double width, height;
+        int dots_count;
+        double Radius;
+        Dots center= new Dots();
+        public class Dots
+        {
+            public double x;
+            public double y;
+        }
+         public void DrawTriangle(Dots point1, Dots point2, Dots point3, double w, double h)
+         {
+             Pen vertex = new Pen(Color.Red, 1);
+            // Используя середину w/2 и h/2
+            //g.DrawLine(vertex, (float)(w / 2 + point1.x), (float)(h / 2 + point1.y), (float)(w / 2 + point2.x), (float)(h / 2 + point2.y));
+            //g.DrawLine(vertex, (float)(w / 2 + point2.x), (float)(h / 2 + point2.y), (float)(w / 2 + point3.x), (float)(h / 2 + point3.y));
+            //g.DrawLine(vertex, (float)(w / 2 + point3.x), (float)(h / 2 + point3.y), (float)(w / 2 + point1.x), (float)(h / 2 + point1.y));
+            g.DrawLine(vertex, (float)(point1.x), (float)(point1.y), (float)(point2.x), (float)(point2.y));
+            g.DrawLine(vertex, (float)(point2.x), (float)(point2.y), (float)(point3.x), (float)(point3.y));
+            g.DrawLine(vertex, (float)(point3.x), (float)(point3.y), (float)(point1.x), (float)(point1.y));
+        }
+
+         public void DrawOutCircle(Dots point1, Dots point2, Dots point3, double w, double h)
+          {
+              Pen vertexPen = new Pen(Color.Green, 3);
+
+              double w1 = ((point2.x * point2.x - point1.x * point1.x + point2.y * point2.y - point1.y * point1.y) * (point3.y - point1.y) - (point3.x * point3.x - point1.x * point1.x + point3.y * point3.y - point1.y * point1.y) * (point2.y - point1.y)) / ((point2.x - point1.x) * (point3.y - point1.y) - (point3.x - point1.x) * (point2.y - point1.y)) / 2.0;
+              double w2 = ((point3.x * point3.x - point1.x * point1.x + point3.y * point3.y - point1.y * point1.y) * (point2.x - point1.x) - (point2.x * point2.x - point1.x * point1.x + point2.y * point2.y - point1.y * point1.y) * (point3.x - point1.x)) / ((point2.x - point1.x) * (point3.y - point1.y) - (point3.x - point1.x) * (point2.y - point1.y)) / 2.0;
+              double Rad = Math.Sqrt((w1 - point1.x) * (w1 - point1.x) + (w2 - point1.y) * (w2 - point1.y));
+              //сохраняем центр и радиус
+              center.x = w1;
+              center.y = w2;
+              Radius = Rad;
+             
+          }
+         public void DrawOutCircle1(Dots point1, Dots point2, Dots point3, double w, double h)
+         {
+             Pen vertexPen = new Pen(Color.Green, 1);
+
+             double w1 = ((point2.x * point2.x - point1.x * point1.x + point2.y * point2.y - point1.y * point1.y) * (point3.y - point1.y) - (point3.x * point3.x - point1.x * point1.x + point3.y * point3.y - point1.y * point1.y) * (point2.y - point1.y)) / ((point2.x - point1.x) * (point3.y - point1.y) - (point3.x - point1.x) * (point2.y - point1.y)) / 2.0;
+             double w2 = ((point3.x * point3.x - point1.x * point1.x + point3.y * point3.y - point1.y * point1.y) * (point2.x - point1.x) - (point2.x * point2.x - point1.x * point1.x + point2.y * point2.y - point1.y * point1.y) * (point3.x - point1.x)) / ((point2.x - point1.x) * (point3.y - point1.y) - (point3.x - point1.x) * (point2.y - point1.y)) / 2.0;
+             double Rad = Math.Sqrt((w1 - point1.x) * (w1 - point1.x) + (w2 - point1.y) * (w2 - point1.y));
+             if (checkBox1.Checked)
+             {
+                 g.DrawEllipse(vertexPen, (float)(w / 2 + (w1 - Rad)), (float)(h / 2 + (w2 - Rad)), (float)(2.0 * Rad), (float)(2.0 * Rad));
+             }
+         }
+        private void gennods_Click(object sender, EventArgs e)
+        {
+            dots_count = Convert.ToInt32(Count.Text); 
+            //Рисование 
+            bmp = new Bitmap(picture.Width, picture.Height);
+            g = Graphics.FromImage(bmp);
+            SolidBrush brush_back = new SolidBrush(Color.White);
+
+            g.FillRectangle(brush_back, 0, 0, picture.Width, picture.Height);
+            width = picture.Width;
+            height = picture.Height;
+
+            mas_dots = new Dots[dots_count];
+
+            //Создание объекта для генерации чисел
+            Random rnd = new Random();
+
+            //Получить случайное число
+            for (int i=0;i<dots_count;i++)
+            {
+                mas_dots[i] = new Dots();
+                mas_dots[i].x= rnd.NextDouble()* width;
+                mas_dots[i].y = rnd.NextDouble() * height;
+            }
+            //ручка для пружины
+            SolidBrush dots = new SolidBrush(Color.Black);
+            for (int i = 0; i < dots_count; i++)
+            {
+                //Rectangle rect = new Rectangle((int)(width / 2+ mas_dots[i].x), (int)(height / 2+ mas_dots[i].y), 3, 3);
+                Rectangle rect = new Rectangle((int)( mas_dots[i].x), (int)( mas_dots[i].y), 3, 3);
+                g.FillRectangle(dots, rect);
+            }
+           
+            //DrawTriangle(mas_dots[0], mas_dots[1], mas_dots[2], width, height);
+            //g.DrawLine(pruzhin, (float)(10), (float)(height / 2 + 10), (float)(width / 2 + X), (float)(height / 2 + 10));
+
+            picture.Image = bmp;
+        }
+
+        private void Joindots_Click(object sender, EventArgs e)
+        {
+            for (int k = 0; k < dots_count ; k++)
+            {
+                for (int j = 0; j < dots_count; j++)
+                {
+                    if (j == k) continue;
+                    for (int i =0; i < dots_count; i++)
+                    {
+                        if (i == j || i == k) continue;
+                        bool a = false;
+                        DrawOutCircle(mas_dots[k], mas_dots[j], mas_dots[i], width, height);
+                        for (int m = 0; m < dots_count; m++)
+                        {
+                            //радиус каждой точки от центра конкретного треугольника 
+                            double R = Math.Sqrt((center.x - mas_dots[m].x) * (center.x - mas_dots[m].x) + (center.y - mas_dots[m].y) * (center.y - mas_dots[m].y));
+
+                            if (R < Radius)
+                            {
+                                a = true;
+                                //DrawTriangle(mas_dots[i], mas_dots[i + 1], mas_dots[i + 2], width, height);
+                            }
+                        }
+                        if (false == a)
+                        {
+                            DrawTriangle(mas_dots[k], mas_dots[j], mas_dots[i], width, height);
+                            DrawOutCircle1(mas_dots[k], mas_dots[j], mas_dots[i], width, height);
+                        }
+                    }
+                }
+            }
+
+            picture.Image = bmp;
+        }
+    }
+
+
+  
+}
